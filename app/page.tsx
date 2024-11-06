7bbd84beef7ec0a57b3585570b4d4e5c6c0a0c2d
xadavm <alfrejimenezgonzalez@gmail.com>
2024-11-06 17:03:20 +0000

    ,m

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
-import Image from "next/image";
+import http from 'http';
+import httpProxy from 'http-proxy';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-      </main>
-    </div>
-  );
-}
+// Crea una instancia del proxy
+const proxy = httpProxy.createProxyServer({});
+
+// Define el servidor de destino (donde se enviarán las solicitudes)
+const target = 'https://nextjs-boilerplate-b30tl5rzy-sofias-projects-94a064b9.vercel.app/'; // Cambia esto a tu servidor de destino
+
+// Crea el servidor que actuará como proxy
+const server = http.createServer((req, res) => {
+  console.log(`Proxying request for ${req?.url}`);
+  proxy.web(req, res, { target }, (err) => {
+    console.error(`Error en la solicitud proxy: ${err?.message}`);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Error en el servidor proxy');
+  });
+});
+
+//
